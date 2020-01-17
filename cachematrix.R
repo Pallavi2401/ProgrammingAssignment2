60af5afa0285ed27a2b2274507353bfac594a269
Pallavi2401 <59971788+Pallavi2401@users.noreply.github.com>
2020-01-17 21:07:22 +0530

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -3,15 +3,15 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-inv <- NULL
-        set <- function(y) {
+makeCacheMatrix <- function(x = matrix()) { ## defining argument with "matrix"
+inv <- NULL                                 ## initializing inv as NULL to hold value of inverse matrix
+        set <- function(y) {                ## defining the set function to assign new value of matrix in parent environment
                 x <<- y
                 inv <<- NULL
         }
-        get <- function() x
-        setinv <- function(inverse) inv <<- inverse
-        getinv <- function() inv
+        get <- function() x                 ## returns value of the matrix argumen
+        setinv <- function(inverse) inv <<- inverse ## assigns value of inv in parent environment
+        getinv <- function() inv                    ## gets the value of inv where called
         list(set = set, get = get,
              setinv = setinv,
              getinv = getinv)
@@ -22,14 +22,14 @@ inv <- NULL
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-inv <- x$getinv()
-        if(!is.null(inv)) {
+inv <- x$getinv()                 
+        if(!is.null(inv)) {                     ## checks whether inverse matrix is not null
                 message("getting cached data")
-                return(inv)
+                return(inv)                     ## returns inverse matrix
         }
-        data <- x$get()
-        inv <- solve(data, ...)
-        x$setinv(inv)
-        inv
+        data <- x$get()                         ## get original matrix
+        inv <- solve(data, ...)                 ## using solve to get inverse of matrix
+        x$setinv(inv)                           ## set inverse matrix
+        inv                                     ## returns the inverse matrix
 }
 }
